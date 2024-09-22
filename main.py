590cc7b4657eab5ce47f9bdd7741498644d0305d
GhostNikolay <snl-10@mail.ru>
2024-09-22 09:05:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ def get_multiplied_digits(number):
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
-    else:
+    elif len(str_number) < 2:
+
         return first
+
+
 result = get_multiplied_digits(40203)
 print(result)
